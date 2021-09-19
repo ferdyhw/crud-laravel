87932edf37d1052273083c1f3f7b686895c53f80
FerdyHw <ferdyhwn@gmail.com>
2021-09-19 19:15:51 +0700

    Mengubah file KomikController

--- app/Http/Controllers/KomikController.php
+++ app/Http/Controllers/KomikController.php
@@ -21,26 +21,49 @@ class KomikController extends Controller
     public function tambah()
     {
         $data = [
-            'judul' => 'Tambah Komik',
+            'judul' => 'Tambah Data Komik',
         ];
         return view('komik.create', $data);
     }
 
     public function prosesTambah(Request $request)
     {
-        // Blom jadi cok
-        // $request->validate([
-        //     'judul' => 'required',
-        //     'slug'  => 'required',
-        //     'penulis'   => 'required',
-        //     'penerbit'  => 'required',
-        //     'sampul'    => 'sampul'
-        // ]);
-        // Komik::create($request->all());
+        $judul = $request->judul;
+        $slug = str_replace(' ', '-', strtolower($judul));
 
-        // echo '<pre>';
-        // print_r($request);
-        // die;
+        Komik::create([
+            'judul' => $request->judul,
+            'slug' => $slug,
+            'genre_id' => mt_rand(1, 3),
+            'penulis' => $request->penulis,
+            'penerbit' => $request->penerbit,
+            'sampul' => $request->sampul
+        ]);
+
+        return redirect('/komik')->with('status', 'Data komik berhasil ditambahkan.');
+    }
+
+    public function ubah(Komik $post)
+    {
+        $data = [
+            'judul' => 'Ubah Data Komik',
+            'komik' => $post,
+        ];
+        return view('komik.update', $data);
+    }
+
+    public function prosesUbah(Request $request, Komik $post)
+    {
+        DB::table('komiks')->where('slug', $post->slug)->update([
+            'genre_id' => $request->genre_id,
+            'judul' => $request->judul,
+            'slug' => $request->slug,
+            'penulis' => $request->penulis,
+            'penerbit' => $request->penerbit,
+            'sampul' => $request->sampul
+        ]);
+
+        return redirect('/komik')->with('status', 'Data komik berhasil diubah.');
     }
 
     public function detail(Komik $post)
@@ -52,5 +75,12 @@ class KomikController extends Controller
         // dd($data);
 
         return view('komik.detail', $data);
+    }
+
+    public function hapus(Komik $post)
+    {
+        DB::table('komiks')->where('id', $post->id)->delete();
+
+        return redirect('/komik')->with('status', 'Data komik berhasil dihapus.');
     }
 }
